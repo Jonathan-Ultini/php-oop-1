b24e0526afd364a4af0bf39769ea76d0ff0531e1
Jonathan-Ultini <jonathanultini@gmail.com>
2024-10-09 16:38:54 +0200

    bonus 1 - add genres

--- classes/Movie.php
+++ classes/Movie.php
@@ -6,18 +6,29 @@ class Movie
   public $title;
   public $director;
   public $year;
+  public $genres;
 
   // Costruttore per inizializzare le proprietà
-  public function __construct($title, $director, $year)
+  public function __construct($title, $director, $year, $genres = [])
   {
     $this->title = $title;
     $this->director = $director;
     $this->year = $year;
+    $this->genres = $genres;
+  }
+
+  // Metodo per ottenere una lista dei generi come stringa
+  public function getGenres()
+  {
+    return implode(", ", $this->genres); // Converte l'array in una stringa separata da virgole
   }
 
   // Metodo per ottenere informazioni sul film
   public function getMovieInfo()
   {
-    return "Title: " . $this->title . ", Director: " . $this->director . ", Year: " . $this->year;
+    return "Title: " . $this->title .
+      ", Director: " . $this->director .
+      ", Year: " . $this->year .
+      ", Genres: " . $this->getGenres();
   }
 }
--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 require_once __DIR__ . '/classes/Movie.php';
 
 // Istanziamento di due oggetti Movie
-$movie1 = new Movie("Il cosmo sul comó", "Marcello Cesena", 2008);
-$movie2 = new Movie("Replicas", "Jeffrey Nachmanoff", 2019);
+$movie1 = new Movie("Il cosmo sul comó", "Marcello Cesena", 2008, ["Commedy"]);
+$movie2 = new Movie("Replicas", "Jeffrey Nachmanoff", 2019, ["Sci-Fi", "Thriller"]);
 
 // Stampa dei valori delle proprietà degli oggetti
 echo $movie1->getMovieInfo();
